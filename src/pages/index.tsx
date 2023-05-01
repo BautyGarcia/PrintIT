4f70f4ef3599ec824fb5d87e959f2b0d0b3b3cc5
Bautista Garcia <bautyroccogarcia@gmail.com>
2023-05-01 16:32:11 -0300

    added google login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -60,7 +60,7 @@ export default Home;
 
 const AuthShowcase: React.FC = () => {
   const { data: sessionData } = useSession();
-
+  
   const { data: secretMessage } = api.example.getSecretMessage.useQuery(
     undefined, // no input
     { enabled: sessionData?.user !== undefined },

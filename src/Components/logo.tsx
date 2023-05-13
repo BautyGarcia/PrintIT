cb83d134cbda4c7c487fa837e54582e94ac9aedd
FrancoBugnaaa <101563222+FrancoBugnaaa@users.noreply.github.com>
2023-05-12 23:11:17 -0300

    uplodaing logos

---
+++ src/Components/logo.tsx
@@ -0,0 +1,6 @@
+import Image from "next/image"
+
+export const Logo = () => {
+    return <Image src= "../../public/Logo.png" alt= "logo"/>
+   
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import { signOut, useSession } from "next-auth/react";
 import { api } from "~/utils/api";
 import Link from "next/link";
+import { Logo } from "~/Components/logo"; 
 
 const Landing: NextPage = () => {
   const { data: sessionData } = useSession();
@@ -14,14 +15,15 @@ const Landing: NextPage = () => {
 
   return (
     <>
-      <Head>
+      <Head> 
+        <link rel="icon" href="../../public/Logo.png" />
         <title>PrintIT</title>
         <meta name="description" content="PrintIT" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl p-5">PrintIT</h1>
         <button>
-          <p className="bg-blue-500 text-white py-2 px-4 rounded"><Link href={"/signUp"}>Enter</Link></p>
+          <p className="bg-blue-500 text-white py-2 px-4 rounded"><Link href={"/signup"}>Enter</Link></p>
         </button>
         {sessionData && <button onClick={() => void signOut()} className="p-5"><p className="bg-blue-500 text-white py-2 px-4 rounded">SignOut</p></button>}
 

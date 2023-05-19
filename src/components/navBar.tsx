f37bf8e46f2bb2dfad8e54dcec54cd9bee6eaac5
FrancoBugnaaa <101563222+FrancoBugnaaa@users.noreply.github.com>
2023-05-18 23:29:02 -0300

    feat(navBar): applied home and landing none redirect of buttons

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,51 +1,56 @@
 import { Logo } from "./logo";
 import Link from "next/link";
 
-interface Links{
-    label : string;
-    router : string;
+interface Links {
+  label: string;
+  router: string;
 }
 
-const Links = [{
+const Links = [
+  {
     label: "Sobre Nosotros",
-    route: "/about"
-},
-{
+    route: "/about",
+  },
+  {
     label: "Inicio",
-    route: "/home"
-},
-{
+    route: "/home",
+  },
+  {
     label: "Contactanos",
-    route: "/contact-us"
-}
-]
+    route: "/contact-us",
+  },
+];
 
 export const NavBar: React.FC = ({}) => {
-    return (
-        <header className="rounded p-4 border-2 border-black w-full">
-            <nav className="flex gap-16 justify-between items-center list-none">
-
-                <div className="w-350 flex gap-4 pl-50 items-center text-center mt-5 mb-5">
-                    <Logo width={40} height={40} display="flex" justifyContent="center" />
-                    <h1 className="font-family-Inter font-weight-250 felx justify-center">PrintIT</h1>
-                </div>
-                <div className="flex w-full justify-around items-center">
-                {Links.map(({ label, route }: { label: string, route: string }) => (
-                    <Link className="color-black text-underline" key={route} href={route}>
-                        {label}
-                    </Link>
-                ))}
-                </div>
-                <div>
-                    <button className="font-family-Inter mt-3 w-4/5 rounded-lg bg-white py-2 text-black hover:bg-blue-700">
-
-                    </button>
-                    <button className="font-family-Inter mt-3 w-4/5 rounded-lg bg-blue-500 py-2 text-white hover:bg-blue-700">
-
-                    </button>
-                </div>
-            </nav>
-
-        </header>
-    )
-}
+  return (
+    <header className="w-full rounded border-2 border-black p-4">
+      <nav className="flex list-none items-center justify-between gap-16">
+        <div className="w-350 pl-50 mb-3 mt-3 flex items-center gap-4 text-center">
+          <Logo width={40} height={40} display="flex" justifyContent="center" />
+          <h1 className="font-family-Inter font-weight-250 felx justify-center">
+            PrintIT
+          </h1>
+        </div>
+        <div className="flex w-full items-center justify-evenly">
+          {Links.map(({ label, route }: { label: string; route: string }) => (
+            <Link
+              className="color-black text-underline"
+              key={route}
+              href={route}
+            >
+              {label}
+            </Link>
+          ))}
+        </div>
+        <div className="felx flex-col items-center justify-center">
+          <button className="font-family-Inter mt-3 w-4/5 rounded-lg border-2 border-black bg-white py-2 text-black hover:bg-gray-500">
+            Iniciar Sesion
+          </button>
+          <button className="font-family-Inter mt-3 w-4/5 rounded-lg border-none bg-blue-500 py-2 text-white hover:bg-blue-700">
+            Registrarse
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 import { signOut, useSession } from "next-auth/react";
 import { api } from "~/utils/api";
 import Link from "next/link";
-import { Logo } from "~/components/logo"; 
+import { Logo } from "~/components/logo";
 import { NavBar } from "~/components/navBar";
 
 const Landing: NextPage = () => {
@@ -11,28 +11,41 @@ const Landing: NextPage = () => {
 
   const { data: secretMessage } = api.example.getSecretMessage.useQuery(
     undefined,
-    { enabled: sessionData?.user !== undefined },
+    { enabled: sessionData?.user !== undefined }
   );
 
   return (
     <>
-      <Head> 
+      <Head>
         <title>PrintIT</title>
-        <link rel="icon" href="/Logo.ico"/>
+        <link rel="icon" href="/Logo.ico" />
         <meta name="description" content="PrintIT" />
       </Head>
       <NavBar />
       <main className="flex min-h-screen flex-col items-center justify-center bg-white from-[#2e026d] to-[#15162c]">
-        <Logo height={50} width={50} justifyContent={"center"} display={"flex"} />
-        <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl p-5">PrintIT</h1>
+        <Logo
+          height={50}
+          width={50}
+          justifyContent={"center"}
+          display={"flex"}
+        />
+        <h1 className="p-5 text-4xl font-bold tracking-tight text-white sm:text-6xl">
+          PrintIT
+        </h1>
         <button>
-          <p className="bg-blue-500 text-white py-2 px-4 rounded"><Link href={"/signUp"}>Enter</Link></p>
+          <p className="rounded bg-blue-500 px-4 py-2 text-white">
+            <Link href={"/signUp"}>Enter</Link>
+          </p>
         </button>
-        {sessionData && <button onClick={() => void signOut()} className="p-5"><p className="bg-blue-500 text-white py-2 px-4 rounded">SignOut</p></button>}
+        {sessionData && (
+          <button onClick={() => void signOut()} className="p-5">
+            <p className="rounded bg-blue-500 px-4 py-2 text-white">SignOut</p>
+          </button>
+        )}
 
         <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
+          {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
+          {secretMessage && <span> - {secretMessage}</span>}
         </p>
       </main>
     </>

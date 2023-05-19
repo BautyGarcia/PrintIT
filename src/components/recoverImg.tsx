96f39eafc390cff61bfeafb418fc5aad31fce688
Bautista Garcia <bautyroccogarcia@gmail.com>
2023-05-19 20:16:10 -0300

    fix(recoverImg.tsx): changed some attributes

--- src/components/recoverImg.tsx
+++ src/components/recoverImg.tsx
@@ -1,10 +1,5 @@
 import Image from "next/image"
 
-interface LogoGoogleProps {
-    width: number;
-    height: number;
-}
-
-export const RecoverIMG: React.FC<LogoGoogleProps> = ({ width, height }) => {
-    return <Image src= "/Recover.png" alt= "logoRecover" width={width} height={height}/>
+export const RecoverIMG: React.FC = () => {
+    return <Image className="h-screen w-full" src= "/Recover.png" alt= "logoRecover" width={680} height={880}/>
 }

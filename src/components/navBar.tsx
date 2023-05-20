2dbc617dd3cfc1a1d1cf00d6acc32dbc6b4453c4
Bautista Garcia <bautyroccogarcia@gmail.com>
2023-05-19 23:33:25 -0300

    feat(navBar): edited Mantine UI navBar

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -4,29 +4,16 @@ import {
   Header,
   Group,
   Button,
-  UnstyledButton,
-  Text,
-  ThemeIcon,
   Divider,
-  Center,
   Box,
   Burger,
   Drawer,
-  Collapse,
   ScrollArea,
   rem,
 } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
-import {
-  IconNotification,
-  IconCode,
-  IconBook,
-  IconChartPie3,
-  IconFingerprint,
-  IconCoin,
-  IconChevronDown,
-} from '@tabler/icons-react';
 import { Logo } from './logo';
+import Link from 'next/link';
 
 const useStyles = createStyles((theme) => ({
   link: {
@@ -87,83 +74,37 @@ const useStyles = createStyles((theme) => ({
   },
 }));
 
-const mockdata = [
-  {
-    icon: IconCode,
-    title: 'Open source',
-    description: 'This Pokémon’s cry is very loud and distracting',
-  },
-  {
-    icon: IconCoin,
-    title: 'Free for everyone',
-    description: 'The fluid of Smeargle’s tail secretions changes',
-  },
-  {
-    icon: IconBook,
-    title: 'Documentation',
-    description: 'Yanma is capable of seeing 360 degrees without',
-  },
-  {
-    icon: IconFingerprint,
-    title: 'Security',
-    description: 'The shell’s rounded shape and the grooves on its.',
-  },
-  {
-    icon: IconChartPie3,
-    title: 'Analytics',
-    description: 'This Pokémon uses its flying ability to quickly chase',
-  },
-  {
-    icon: IconNotification,
-    title: 'Notifications',
-    description: 'Combusken battles with the intensely hot flames it spews',
-  },
-];
-
 export const NavBar: React.FC = () => {
   const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] = useDisclosure(false);
-  const [linksOpened, { toggle: toggleLinks }] = useDisclosure(false);
   const { classes, theme } = useStyles();
-
-  const links = mockdata.map((item) => (
-    <UnstyledButton className={classes.subLink} key={item.title}>
-      <Group noWrap align="flex-start">
-        <ThemeIcon size={34} variant="default" radius="md">
-          <item.icon size={rem(22)} color={theme.fn.primaryColor()} />
-        </ThemeIcon>
-        <div>
-          <Text size="sm" fw={500}>
-            {item.title}
-          </Text>
-          <Text size="xs" color="dimmed">
-            {item.description}
-          </Text>
-        </div>
-      </Group>
-    </UnstyledButton>
-  ));
 
   return (
     <Box pb={120}>
-      <Header height={60} px="md">
+      <Header height={60} px="md" className="fixed">
         <Group position="apart" sx={{ height: '100%' }}>
-          <Logo width={40} height={40}/>
+          <Link href="/" passHref>
+            <Logo width={40} height={40}/>
+          </Link>
 
           <Group sx={{ height: '100%' }} spacing={0} className={classes.hiddenMobile}>
-            <a href="#" className={classes.link}>
-              Home
-            </a>
-            <a href="#" className={classes.link}>
-              Learn
-            </a>
-            <a href="#" className={classes.link}>
-              Academy
-            </a>
+            <Link href="/about" className={classes.link}>
+              Sobre Nosotros
+            </Link>
+            <Link href="/" className={classes.link}>
+              Inicio
+            </Link>
+            <Link href="/contact-us" className={classes.link}>
+              Contactanos
+            </Link>
           </Group>
 
           <Group className={classes.hiddenMobile}>
-            <Button variant="default">Log in</Button>
-            <Button variant='undefined' className='bg-indigo-400 text-white'>Sign up</Button>
+            <Link href="/signIn" passHref>
+              <Button variant="default">Log in</Button>
+            </Link>
+            <Link href="/signUp" passHref>
+              <Button variant='undefined' className='bg-indigo-400 text-white'>Sign up</Button>
+            </Link>
           </Group>
 
           <Burger opened={drawerOpened} onClick={toggleDrawer} className={classes.hiddenDesktop} />
@@ -182,30 +123,25 @@ export const NavBar: React.FC = () => {
         <ScrollArea h={`calc(100vh - ${rem(60)})`} mx="-md">
           <Divider my="sm" color={theme.colorScheme === 'dark' ? 'dark.5' : 'gray.1'} />
 
-          <a href="#" className={classes.link}>
-            Home
-          </a>
-          <UnstyledButton className={classes.link} onClick={toggleLinks}>
-            <Center inline>
-              <Box component="span" mr={5}>
-                Features
-              </Box>
-              <IconChevronDown size={16} color={theme.fn.primaryColor()} />
-            </Center>
-          </UnstyledButton>
-          <Collapse in={linksOpened}>{links}</Collapse>
-          <a href="#" className={classes.link}>
-            Learn
-          </a>
-          <a href="#" className={classes.link}>
-            Academy
-          </a>
+          <Link href="/about" className={classes.link}>
+            Sobre Nosotros
+          </Link>
+          <Link href="/" className={classes.link}>
+            Inicio
+          </Link>
+          <Link href="/contact-us" className={classes.link}>
+            Contactanos
+          </Link>
 
           <Divider my="sm" color={theme.colorScheme === 'dark' ? 'dark.5' : 'gray.1'} />
 
           <Group position="center" grow pb="xl" px="md">
-            <Button variant="default">Log in</Button>
-            <Button variant="default">Sign up</Button>
+            <Link href="/signIn" passHref>
+              <Button variant="default">Log in</Button>
+            </Link>
+            <Link href="/signUp" passHref>
+              <Button variant='undefined' className='bg-indigo-400 text-white'>Sign up</Button>
+            </Link>
           </Group>
         </ScrollArea>
       </Drawer>

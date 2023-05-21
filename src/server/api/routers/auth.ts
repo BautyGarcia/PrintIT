0d3c706dcc57ac50d4805ba0fc37d633f9458d95
Bautista Garcia <bautyroccogarcia@gmail.com>
2023-05-20 23:31:05 -0300

    feat(recoverPasswrod): added embedded email for password recovering

--- src/server/api/routers/auth.ts
+++ src/server/api/routers/auth.ts
@@ -19,6 +19,79 @@ const transporter = nodemailer.createTransport({
     }
 });
 
+const emailText = (username: string, redirectURL: string) => `
+    <!DOCTYPE html>
+
+    <html>
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Password Recovery</title>
+            <style>
+                body {
+                margin: 0;
+                padding: 0;
+                font-family: Arial, sans-serif;
+                background-color: #f2f2f2;
+                }
+
+                .container {
+                max-width: 600px;
+                margin: 0 auto;
+                background-color: #ffffff;
+                border-radius: 4px;
+                overflow: hidden;
+                box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+                }
+
+                .header {
+                padding: 20px;
+                background-color: #007bff;
+                color: #ffffff;
+                text-align: center;
+                }
+
+                .logo {
+                max-width: 50px;
+                margin: 0 auto;
+                display: block;
+                }
+
+                .content {
+                padding: 20px;
+                text-align: center;
+                }
+
+                .button {
+                display: inline-block;
+                padding: 10px 20px;
+                background-color: #007bff;
+                color: #ffffff;
+                text-decoration: none;
+                border-radius: 4px;
+                }
+            </style>
+        </head>
+        <body>
+            <div class="container">
+                <div class="header">
+                    <img src="cid:logoBlanco" alt="App Logo" class="logo">
+                </div>
+                <div class="content">
+                    <h1>Recuperación de contraseña</h1>
+                    <p>Hola <b>${username}</b>,</p>
+                    <p>Hemos recibido una solicitud para restablecer tu contraseña. Para continuar, por favor haz clic en el siguiente botón:</p>
+                    <a href="${redirectURL}" class="button">Restablecer Contraseña</a>
+                    <p>Si no solicitaste restablecer la contraseña, por favor ignora este correo electrónico.</p>
+                    <p>Gracias,</p>
+                    <p>El equipo de PrintIT</p>
+                </div>
+            </div>
+        </body>
+    </html>
+`
+
+
 export const authRouter = createTRPCRouter({
     recoverPassword: publicProcedure
         .input(z.object({ email: z.string() }))
@@ -51,12 +124,21 @@ export const authRouter = createTRPCRouter({
                 },
             });
 
+            const emailName = email.slice(0, email.indexOf('@'));
+            const redirectURL = env.NODE_ENV === "production" ? `https://printitweb.vercel.app/recoverPassword/${token}` : `http://localhost:3000/recoverPassword/${token}`;
             //Set mail options
             const mailOptions = {
-                from: env.MAILUSER,
+                from: 'PrintIT <contact.printit.app@gmail.com>',
                 to: email,
-                subject: "Password Recovery",
-                text: `You have requested to recover your password. Please click on the following link to reset your password: http://localhost:3000/recoverPassword/${token}`,
+                subject: 'PrintIT - Olvidaste tu contraseña',
+                html: emailText(emailName, redirectURL),
+                attachments: [
+                    {
+                        filename: 'LogoWhite.png',
+                        path: 'public/LogoWhite.png',
+                        cid: 'logoBlanco' //same cid value as in the html img src
+                    }
+                ]
             };
 
             //Send Email

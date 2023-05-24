073dff873fa55f394e645bf5b2a2ed3297c86360
Bautista Garcia <bautyroccogarcia@gmail.com>
2023-05-23 23:17:11 -0300

    feat(dropzone): included .stl validation function

--- src/components/fileDropzone.tsx
+++ src/components/fileDropzone.tsx
@@ -1,9 +1,40 @@
 import { Text } from '@mantine/core';
-
+import { type ChangeEvent } from 'react';
 const STLDropzone = () => {
+
+    const handleFileSubmit = (event: ChangeEvent<HTMLInputElement>) => {
+        const files = event.target.files;
+
+        if (!files || files.length === 0 || !files[0]) {
+            console.log('No files selected.');
+            return;
+        }
+
+        const file = files[0];
+
+        // Check file extension
+        const allowedExtensions = ['stl', "STL"];
+        const fileExtension = file.name.split('.').pop()?.toLowerCase() as string;
+        if (!allowedExtensions.includes(fileExtension)) {
+            console.log('Invalid file extension. Only .stl files are allowed.');
+            return;
+        }
+
+        // Check file size
+        const maxSize = 60 * 1024 * 1024; // 60MB
+        if (file.size > maxSize) {
+            console.log('File size exceeds the allowed limit.');
+            return;
+        }
+
+        console.log('Uploading file...');
+    }
+
+
     return (
         <div>
             <Text>Dropzone</Text>
+            <input type="file" onChange={handleFileSubmit} />
         </div>
     );
 }

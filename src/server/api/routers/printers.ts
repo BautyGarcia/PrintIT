8e781be12954d9def685cc596d8aaa5cba6c0a73
BautyGarcia <47026765@est.ort.edu.ar>
2023-06-22 15:03:50 -0300

    feat(printers router): started getPrintersForSTL and getMyPrinters procedures

--- src/server/api/routers/printers.ts
+++ src/server/api/routers/printers.ts
@@ -31,4 +31,49 @@ export const printerRouter = createTRPCRouter({
 
             return newPrinter;
         }),
+    getPrinterForSTL: publicProcedure
+        .input(z.object({ bedSize: z.string(), printerType: z.string() }))
+        .query(async ({ input, ctx }) => {
+            const { bedSize, printerType } = input;
+
+            const printers = await ctx.prisma.printer.findMany({
+                where: {
+                    type: printerType,
+                }
+            })
+
+            if (!printers) {
+                return null;
+            }
+
+            const printersForSTL = printers.filter((printer) => {
+                const printerBedSize = printer.bedSize.split("x");
+                const stlBedSize = bedSize.split("x");
+
+                if (Number(printerBedSize[0]) >= Number(stlBedSize[0]) && Number(printerBedSize[1]) >= Number(stlBedSize[1]) && Number(printerBedSize[2]) >= Number(stlBedSize[2])) {
+                    return true;
+                }
+
+                return false;
+            })
+
+            return printers;
+        }),
+    getMyPrinters: publicProcedure
+        .input(z.object({ userId: z.string() }))
+        .query(async ({ input, ctx }) => {
+            const { userId } = input;
+
+            const printers = await ctx.prisma.printer.findMany({
+                where: {
+                    userId: userId,
+                }
+            })
+
+            if (!printers) {
+                return null;
+            }
+
+            return printers;
+        }),
 })

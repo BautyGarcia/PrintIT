299cbc9b2f248224d6c7b3031438d54952bdf8ad
BautistaMalu <89524797+BautistaMalu@users.noreply.github.com>
2023-06-27 10:40:54 -0300

    feat(AboutUs) images added
    images,padding and margins added

---
+++ src/components/linkedinlogo.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export const LinkedinLOGO: React.FC = () => {
+  return (
+    <Image
+      
+      src="/LinkedinLogo.png"
+      alt="Linkedin Logo"
+      width={680}
+      height={880}
+    />
+  );
+};

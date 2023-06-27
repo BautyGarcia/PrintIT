7d538b9d1f1a18ff78b0069b228be4528db0498a
FrancoBugnaaa <101563222+FrancoBugnaaa@users.noreply.github.com>
2023-06-27 08:13:34 -0300

    feat(error unknown)

--- src/components/settingsHeader.tsx
+++ src/components/settingsHeader.tsx
@@ -17,31 +17,6 @@ import UserBox from "./userBox";
 import { useSession } from "next-auth/react";
 
 const useStyles = createStyles((theme) => ({
-  link: {
-    display: "flex",
-    alignItems: "center",
-    height: "100%",
-    paddingLeft: theme.spacing.md,
-    paddingRight: theme.spacing.md,
-    textDecoration: "none",
-    color: theme.colorScheme === "dark" ? theme.white : theme.black,
-    fontWeight: 500,
-    fontSize: theme.fontSizes.sm,
-
-    [theme.fn.smallerThan("sm")]: {
-      height: rem(42),
-      display: "flex",
-      alignItems: "center",
-      width: "100%",
-    },
-
-    ...theme.fn.hover({
-      backgroundColor:
-        theme.colorScheme === "dark"
-          ? theme.colors.dark[6]
-          : theme.colors.gray[0],
-    }),
-  },
 
   dropdownFooter: {
     backgroundColor:

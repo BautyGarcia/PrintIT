c5015a5b88216e25d8fd900e63f566026b41be2d
Bautista Garcia <bautygarcia@Bautistas-MacBook-Pro.local>
2023-11-03 23:09:38 -0300

    fix(carousel): reversed carousel changes due to image rendering issues

--- src/components/Landing/carousel.tsx
+++ src/components/Landing/carousel.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import classNames from "classnames";
 import Image from "next/image";
 
 const images = [
@@ -15,7 +16,7 @@ const Carousel: React.FC = () => {
   useEffect(() => {
     const interval = setInterval(() => {
       setActiveIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 4000); // Change slide every 4 seconds
+    }, 4000); // Change slide every 3 seconds
 
     return () => clearInterval(interval);
   }, []);
@@ -23,20 +24,17 @@ const Carousel: React.FC = () => {
   return (
     <div className="flex h-screen w-full items-center justify-center">
       {images.map((image, index) => (
-        <div
-          key={image}
-          className={`absolute w-full transition-opacity duration-1000 ${
+        <Image
+          key={index}
+          src={image}
+          alt={`Image ${index + 1}`}
+          className={classNames(
+            "absolute w-full transition-opacity duration-1000",
             index === activeIndex ? "opacity-100" : "opacity-0"
-          }`}
-        >
-          <Image
-            src={image}
-            alt={`Image ${index + 1}`}
-            layout="fill"
-            objectFit="cover" // Assuming you want to cover the area of the div
-            priority={index === activeIndex}
-          />
-        </div>
+          )}
+          width={3840}
+          height={2160}
+        />
       ))}
     </div>
   );
